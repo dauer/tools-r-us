d5cb9737345592c609512093de51347c139e3304
Thomas Rasmussen <thomas.bregentved@gmail.com>
2018-08-06 17:38:25 +0200

    Updated Drobo mount script

--- bash/drobo.sh
+++ bash/drobo.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+#
+# sudo apt-get install samba
+# sudo apt-get install cifs-utils
+#
 
-sudo mount -t cifs -o sec=ntlm,uid=1000 //192.168.1.11/Drobo /media/drobo
+sudo mount -t cifs -o vers=1.0,sec=ntlm,uid=1000 //192.168.0.8/Drobo /media/drobo
